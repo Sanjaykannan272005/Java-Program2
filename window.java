4215bfc274183181494bb67e92b2f83308a6eb1a
Sanjay <154648044+Sanjaykannan272005@users.noreply.github.com>
2025-04-01 14:22:38 +0530

    Update window.java

--- window.java
+++ window.java
@@ -1,14 +1,6 @@
-import java.applet.*;
-import java.awt.*;
-public class window extends Applet{
-   String n,a;
-   public void init(){
-       n=getParameter("name");
-       a=getParameter("course");
-   }
-   public void paint(Graphics g)
-   {
-       g.drawString("welcome"+n+"to"+a,20,20);
-       System.out.println("welcome");
-   }
-}    
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Print Hello, World! to the console
+        System.out.println("Hello, World!");
+    }
+}
